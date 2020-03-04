c05bbd3c38ecd8a9306d5775e8a377d2c0615a61
Csaba Almasi <csaba.almasi@cognite.com>
2020-03-04 15:03:15 +0100

    type in shell script and update workflow naming

--- .github/actions/test_python_3_5_0/entrypoint.sh
+++ .github/actions/test_python_3_5_0/entrypoint.sh
@@ -2,5 +2,5 @@
 echo "'---Installing dependencies---'"
 python3 -m pip install --upgrade pip
 pip3 install -r core-requirements.txt
-echo ""'---Running tests---'"
+echo "'---Running tests---'"
 python3 -m pytest tests/tests_unit -m 'not dsl' --test-deps-only-core
