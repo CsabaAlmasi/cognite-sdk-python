5f6793a5f27403cab722abea873bf82bbd78f720
Csaba Almasi <csaba.almasi@cognite.com>
2020-03-04 14:11:20 +0100

    added action test

---
+++ .github/actions/test_python_3_5_0/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "'---Installing dependencies---'"
+python3 -m pip install --upgrade pip
+pip3 install -r core-requirements.txt
+echo ""'---Running tests---'"
+python3 -m pytest tests/tests_unit -m 'not dsl' --test-deps-only-core
